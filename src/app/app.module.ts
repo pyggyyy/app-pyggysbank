579cba73bdf2f811f491a09c7f6180f9ae7e1d17
pyggyyy <143912609+pyggyyy@users.noreply.github.com>
2023-09-02 16:57:48 -0700

    Generate First Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoCreateComponent } from './todo-create/todo-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoCreateComponent
   ],
   imports: [
     BrowserModule
