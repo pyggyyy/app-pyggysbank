fa6f1f0b407c10f228af52e73e61178fe25f9d8a
pyggyyy <143912609+pyggyyy@users.noreply.github.com>
2023-09-04 13:10:49 -0700

    Remove unused imports

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Subscription} from 'rxjs';
 
 import { Todo } from './../todo.model';
