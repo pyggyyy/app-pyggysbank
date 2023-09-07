a20721c8807f0dd6181728c167917787e9949fba
pyggyyy <143912609+pyggyyy@users.noreply.github.com>
2023-09-06 20:42:03 -0700

    npm install --save body-parser, Handle POST middleware on app.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParse.json());
+app.unsubscribe(bodyParser.urlencoded({extended:false}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods','GET, POST. PATCH, DELETE, PUT, OPTIONS');
     next();
+})
+
+app.post('api/todos', (req,res,next) => {
+    const todos = req.body;
+    console.log(todos);
+    res.status(201).json({
+        message: 'Post Added Succesfully'
+    });
 })
 
 app.use('/api/todos',(req, res, next) => {
