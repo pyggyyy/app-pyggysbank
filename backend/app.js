8221a11978284f5d0dce710146983c5b2d734b9f
pyggyyy <143912609+pyggyyy@users.noreply.github.com>
2023-09-06 20:20:01 -0700

    Add API Route for Fetching initial Todos

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,23 @@ const express = require('express');
 
 const app = express();
 
-
-app.use((req, res, next) => {
-    res.send('Hello From Express!')
+app.use('/api/todos',(req, res, next) => {
+    const todos = [
+        {
+            id:'asdf234',
+            title:'First Todo',
+            content: 'coming from server'
+        },
+        {
+            id:'tyrej345',
+            title:'Second Todo',
+            content: 'coming from server again!'
+        }
+    ]
+    res.status(200).json({
+        message: 'Recieved Succesfully',
+        todos: todos
+    });
 });
 
 module.exports = app;
