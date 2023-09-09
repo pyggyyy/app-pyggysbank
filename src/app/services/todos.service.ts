56a4d9404a562dd0cab869fa05823a24d3ed3ea2
pyggyyy <143912609+pyggyyy@users.noreply.github.com>
2023-09-09 11:07:01 -0700

    map _id to id property on front end

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import {Todo} from './../todo.model';
 import {HttpClient} from '@angular/common/http'
 import { Subject } from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class TodoService {
@@ -14,9 +15,18 @@ export class TodoService {
     }
 
     getTodos() {
-        this.http.get<{message: string,todos:Todo[]}>('http://localhost:3000/api/todos')
-        .subscribe((todoData) => {
-            this.todos = todoData.todos;
+        this.http.get<{message: string,todos:any}>('http://localhost:3000/api/todos')
+        .pipe(map((todoData) => {
+            return todoData.todos.map(todo => {
+                return{
+                    title: todo.title,
+                    content: todo.content,
+                    id: todo._id
+                }
+            })
+        }))
+        .subscribe((transformedTodos) => {
+            this.todos = transformedTodos;
             this.todosUpdated.next([...this.todos]);
         });
     }
