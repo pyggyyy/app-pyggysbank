922370b8ade0542d9611701fcf41abb6b035c07e
pyggyyy <143912609+pyggyyy@users.noreply.github.com>
2023-09-14 06:22:30 -0700

    Fill Signup Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { TodoListComponent } from "./todo-list/todo-list.component";
 import { TodoCreateComponent } from "./todo-create/todo-create.component";
 import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
 
 const routes: Routes = [
     {path:'', component: TodoListComponent},
     {path:'create',component:TodoCreateComponent},
     {path:'edit/:todoId',component:TodoCreateComponent},
     {path:'login',component:LoginComponent},
+    {path:'signup',component:SignupComponent},
 ]
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
