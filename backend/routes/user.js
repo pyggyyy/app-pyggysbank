6022dd013383a36f237588fb4eedc9f7dc45968b
pyggyyy <143912609+pyggyyy@users.noreply.github.com>
2023-09-16 09:20:59 -0700

    Add Login Route with JWT on backend
    npm install --save jsonwebtoken

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -26,5 +27,31 @@ router.post('/signup',(req,res,next) => {
         })
     })
 });
+
+router.post('/login',(req,res,next) => {
+    User.find({email:req.body.email})
+    .then(user => {
+        if(!user){
+            return res.status(401).json({
+                message: 'Auth Failed'
+            })
+        }
+        return bcrypt.compare(req.body.password,user.password)
+    })
+    .then(result => {
+        if(!result){
+            return res.status(401).json({
+                message: 'Auth Failed'
+            });
+        }
+        //Create Json Web Token
+        const token = jwt.sign({email:user.email,userId:user._id}, 'secret_this_should_be_longer',{expiresIn:'1h'});
+    })
+    .catch(err => {
+        return res.status(401).json({
+            message: 'Auth Failed'
+        });
+    })
+})
 
 module.exports = router;
