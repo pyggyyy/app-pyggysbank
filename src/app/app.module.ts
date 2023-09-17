5e7bc8f71ff12928813e9870e12ef53905c8b85a
pyggyyy <143912609+pyggyyy@users.noreply.github.com>
2023-09-16 22:49:25 -0700

    Middleware for checking authentication and protecting routes that we want Authorized access only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 
 //Material Modules
 import { MatInputModule } from '@angular/material/input';
@@ -20,6 +20,7 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
 //import { TodoService } from './services/todos.service';
 
 @NgModule({
@@ -46,7 +47,7 @@ import { SignupComponent } from './auth/signup/signup.component';
     MatProgressSpinnerModule,
     MatPaginatorModule
   ],
-  providers: [],
+  providers: [{provide:HTTP_INTERCEPTORS,useClass: AuthInterceptor, multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
