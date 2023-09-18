225abdb518b5ff78f78d00d4c8e263b88ef863d0
pyggyyy <143912609+pyggyyy@users.noreply.github.com>
2023-09-17 23:11:13 -0700

    Fix Pagination Bug
    Limitations with mat-paginator

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import {Subscription} from 'rxjs';
 
 import { Todo } from './../todo.model';
 
 //Import Service
 import { TodoService } from '../services/todos.service';
-import { PageEvent } from '@angular/material/paginator';
+import { PageEvent, MatPaginator } from '@angular/material/paginator';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -25,15 +25,17 @@ export class TodoListComponent implements OnInit, OnDestroy {
   private todosSub: Subscription;
   private authStatusSub: Subscription;
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor(public todosService: TodoService, private authService: AuthService){};
 
   ngOnInit() {
     this.isLoading = true;
-    this.todosService.getTodos(this.todosPerPage,this.currentPage);
+    this.todosService.getTodos(this.todosPerPage, this.currentPage);
     this.todosSub = this.todosService.getTodoUpdateListener()
-    .subscribe((todosData: {todos: Todo[], todoCount: number}) => {
+    .subscribe((todosData: { todos: Todo[], todoCount: number }) => {
       this.isLoading = false;
-      this.totalTodos = todosData.todoCount;
+      this.totalTodos = todosData.todoCount; // Update totalTodos here
       this.todos = todosData.todos;
     });
     this.userIsAuthenticated = this.authService.getIsAuth();
@@ -50,10 +52,16 @@ export class TodoListComponent implements OnInit, OnDestroy {
     this.todosService.getTodos(this.todosPerPage,this.currentPage);
   }
 
-  onDelete(todoId: string){
+  onDelete(todoId: string) {
     this.isLoading = true;
     this.todosService.deleteTodo(todoId).subscribe(() => {
-      this.todosService.getTodos(this.todosPerPage,this.currentPage);
+      if (this.todos.length === 1 && this.totalTodos > 1) {
+        this.currentPage--;
+      }
+      this.todosService.getTodos(this.todosPerPage, this.currentPage);
+      
+      // Manually update the paginator's length property
+      this.paginator.length = this.totalTodos;
     });
   }
 
