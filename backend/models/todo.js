2c53f4264df1de6145fdc24197fe69f886ca129d
pyggyyy <143912609+pyggyyy@users.noreply.github.com>
2023-09-18 22:37:28 -0700

    Add Creator Key to Schema

--- backend/models/todo.js
+++ backend/models/todo.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const todoSchema = mongoose.Schema({
     title: {type: String, required: true},
     content: {type: String, required: false},
-    imagePath: {type: String, required:false}
+    imagePath: {type: String, required:false},
+    creator: {type: mongoose.Schema.Types.ObjectId, ref:"User",required:true}
 })
 
 module.exports = mongoose.model('Todo', todoSchema);
