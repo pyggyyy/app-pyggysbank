4096af28760d4ee9ccc493d4bc7e53a6f99fff57
pyggyyy <143912609+pyggyyy@users.noreply.github.com>
2023-09-22 20:58:19 -0700

    materials module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 
-//Material Modules
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog';
+
 
 import { AppComponent } from './app.component';
 import { TodoCreateComponent } from './todo-create/todo-create.component';
@@ -24,7 +16,10 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
-//import { TodoService } from './services/todos.service';
+
+//Add Materials Module
+import { AngularMaterialModule } from './angular-materials.module';
+
 
 @NgModule({
   declarations: [
@@ -43,14 +38,7 @@ import { ErrorComponent } from './error/error.component';
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule
+    AngularMaterialModule
   ],
   providers: [
     {provide:HTTP_INTERCEPTORS,useClass: AuthInterceptor, multi:true},
